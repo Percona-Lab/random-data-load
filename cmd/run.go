d236a7bbc11293edf5e3470e62e22481fd43a86e
Yoann La Cancellera <lacancellera.yoann@gmail.com>
2025-07-22 11:53:04 +0200

    add: virtual FK, recursive inserts
    current: virtualFK are forced

--- cmd/run.go
+++ cmd/run.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"sync"
+
+	"slices"
 
 	"github.com/apoorvam/goterminal"
 	"github.com/rs/zerolog/log"
@@ -15,14 +18,14 @@ import (
 type RunCmd struct {
 	DB db.Config `embed:""`
 
-	Table    string
-	Rows     int64 `name:"rows" required:"true" help:"Number of rows to insert"`
-	BulkSize int64 `name:"bulk-size" help:"Number of rows per insert statement" default:"1000"`
-	DryRun   bool  `name:"dry-run" help:"Print queries to the standard output instead of inserting them into the db"`
-	Quiet    bool  `name:"quiet" help:"Do not print progress bar"`
+	Table    string `help:"which table to insert to. It will be ignored when a query is included with either --query or --query-file"`
+	Rows     int64  `name:"rows" required:"true" help:"Number of rows to insert"`
+	BulkSize int64  `name:"bulk-size" help:"Number of rows per insert statement" default:"1000"`
+	DryRun   bool   `name:"dry-run" help:"Print queries to the standard output instead of inserting them into the db"`
+	Quiet    bool   `name:"quiet" help:"Do not print progress bar"`
 
-	Query     string
-	QueryFile string
+	Query     string `help:"providing a query will enable to automatically discover the schema, insert recursively into tables, anticipate joins"`
+	QueryFile string `help:"see --query. Accepts a path instead of a direct query"`
 
 	generate.ForeignKeyLinks
 }
@@ -34,26 +37,56 @@ func (cmd *RunCmd) Run() error {
 		return err
 	}
 
-	table, err := db.LoadTable(cmd.DB.Database, cmd.Table)
-	if err != nil {
-		return err
+	tablesNames := map[string]struct{}{}
+	identifiers := map[string]struct{}{}
+	joins := map[string]string{}
+
+	if !cmd.hasQuery() && cmd.Table == "" {
+		return errors.New("Need either a query (--query | --query-file) or a table (--table)")
 	}
 
-	var (
-		tables, identifiers map[string]struct{}
-		joins               map[string]string
-	)
-	if cmd.Query != "" || cmd.QueryFile != "" {
-		tables, identifiers, joins, err = data.ParseQuery(cmd.Query, cmd.QueryFile, cmd.DB.Engine)
+	if cmd.hasQuery() {
+		tablesNames, identifiers, joins, err = data.ParseQuery(cmd.Query, cmd.QueryFile, cmd.DB.Engine)
 		if err != nil {
 			return err
 		}
 		log.Debug().Interface("identifiers", identifiers).Interface("joins", joins).Msg("query parsed")
-		table.SkipBasedOnIdentifiers(identifiers)
-		table.AddVirtualFKs(joins)
 	}
-	_ = tables
-	_, err = cmd.run(table)
+	// if --table is given, we will restrict inserts to this table only
+	// we will still skip some columns and potentially have virtual FKs
+	if cmd.Table != "" {
+		tablesNames = map[string]struct{}{cmd.Table: struct{}{}}
+	}
+
+	tablesSorted := []*db.Table{}
+	for tableKey := range tablesNames {
+		table, err := db.LoadTable(cmd.DB.Database, tableKey)
+		if err != nil {
+			return err
+		}
+
+		if cmd.hasQuery() {
+			table.SkipBasedOnIdentifiers(identifiers)
+		}
+		tablesSorted = append(tablesSorted, table)
+	}
+	db.FilterVirtualFKs(tablesSorted, joins)
+	db.AddVirtualFKs(tablesSorted, joins)
+
+	// sort from the tables with least constraints to table with most constraint
+	// TODO: could disable the FK for which we don't have the table anyway
+	slices.SortFunc(tablesSorted, func(a, b *db.Table) int {
+		return len(a.Constraints) - len(b.Constraints)
+	})
+
+	for _, table := range tablesSorted {
+		log.Debug().Str("table", table.Name).Int("number of constraint", len(table.Constraints)).Msg("tables sorted")
+	}
+
+	for _, table := range tablesSorted {
+		_, err = cmd.run(table)
+	}
+
 	return err
 }
 
@@ -73,6 +106,10 @@ func (cmd *RunCmd) run(table *db.Table) (int64, error) {
 	n, err := ins.Run(cmd.Rows, cmd.BulkSize)
 	wg.Wait()
 	return n, err
+}
+
+func (cmd *RunCmd) hasQuery() bool {
+	return cmd.Query != "" || cmd.QueryFile != ""
 }
 
 func startProgressBar(total int64, c chan int64, wg *sync.WaitGroup) {
--- db/data.go
+++ db/data.go
@@ -197,57 +197,6 @@ func (t *Table) SkipBasedOnIdentifiers(identifiers map[string]struct{}) {
 	}
 }
 
-func (t *Table) AddVirtualFKs(fkeys map[string]string) error {
-	// source and target is in the order of the written query, not necessarily in the logical order
-	// source would be the parent table
-	// target would be the child, which could have had an actual FOREIGN KEY object
-	// so the current t *Table should be the target: it points to a dependency
-	for source, target := range fkeys {
-		sourceTable, sourceCol, ok1 := strings.Cut(source, ".")
-		targetTable, targetCol, ok2 := strings.Cut(target, ".")
-		if !ok1 || !ok2 {
-			log.Debug().Str("key", source).Str("value", target).Str("tablename", t.Name).Str("table schema", t.Schema).Str("func", "AddVirtualFKs").Msg("malformed virtualfk")
-			continue
-		}
-
-		if t.Name == sourceTable { // flipped
-			sourceTable, sourceCol, _ = strings.Cut(target, ".")
-			targetTable, targetCol, _ = strings.Cut(source, ".")
-		} else if t.Name != targetTable { // none matches, so the FK should be created on another *Table
-			continue
-		}
-
-		if _, ok := t.ColToConstraint[targetCol]; ok {
-			continue
-		}
-		constraint := Constraint{
-			ConstraintName:        "VirtualFK_" + targetCol,
-			ReferencedTableSchema: t.Schema, // assuming the schema is the same, good enough for now
-			ReferencedTableName:   sourceTable,
-			ColumnsName:           []string{targetCol},
-			ReferencedColumsName:  []string{sourceCol},
-		}
-		constraint.populateFields(t)
-		err := constraint.loadReferencedTable()
-		if err != nil {
-			return errors.Wrap(err, "AddVirtualFKs")
-		}
-		t.Constraints = append(t.Constraints, constraint)
-	}
-
-	// already called once on the real constraints, but it's safe to call twice to resolve the virtual FKs
-	t.resolveColToConstraints()
-	return nil
-}
-
-func (f *Field) skippeable() bool {
-	log.Debug().Str("field", f.ColumnName).Bool("nullable", f.IsNullable).Bool("hasDefault", f.HasDefaultValue).Str("func", "skippeable").Msg("debug skippeable")
-	if !f.IsNullable && !f.HasDefaultValue {
-		return false
-	}
-	return true
-}
-
 func isSupportedType(fieldType string) bool {
 	supportedTypes := map[string]bool{
 		"tinyint":    true,
@@ -291,4 +240,93 @@ func EscapedNamesListFromFields(fields []Field) string {
 		names = append(names, Escape(field.ColumnName))
 	}
 	return strings.Join(names, ",")
+}
+
+func FilterVirtualFKs(tables []*Table, fkeys map[string]string) {
+	// source and target is in the order of the written query, not necessarily in the logical order
+	// source would be the parent table
+	// target would be the child, which could have had an actual FOREIGN KEY object
+	// so the current t *Table should be the target: it points to a dependency
+	for source, target := range fkeys {
+		sourceTable, sourceCol, ok1 := strings.Cut(source, ".")
+		targetTable, targetCol, ok2 := strings.Cut(target, ".")
+		if !ok1 || !ok2 {
+			log.Debug().Str("key", source).Str("value", target).Str("func", "FilterVirtualFKs").Msg("malformed virtualfk")
+			delete(fkeys, source)
+			continue
+		}
+
+		for _, table := range tables {
+			for _, constraint := range table.Constraints {
+				switch {
+				case sourceTable == table.Name && targetTable == constraint.ReferencedTableName &&
+					slices.Contains(constraint.ColumnsName, sourceCol) && slices.Contains(constraint.ReferencedColumsName, targetCol):
+					delete(fkeys, source)
+
+					// flipped
+				case targetTable == table.Name && sourceTable == constraint.ReferencedTableName &&
+					slices.Contains(constraint.ColumnsName, targetCol) && slices.Contains(constraint.ReferencedColumsName, sourceCol):
+					delete(fkeys, source)
+				}
+
+			}
+		}
+	}
+}
+
+func AddVirtualFKs(tables []*Table, fkeys map[string]string) error {
+	for source, target := range fkeys {
+		sourceTable, sourceCol, _ := strings.Cut(source, ".")
+		targetTable, targetCol, _ := strings.Cut(target, ".")
+
+		var table *Table
+		tableIdx := slices.IndexFunc(tables, func(t *Table) bool { return t.Name == sourceTable })
+		targetTableIdx := slices.IndexFunc(tables, func(t *Table) bool { return t.Name == targetTable })
+		switch {
+		case tableIdx != -1 && targetTableIdx != -1:
+			// sets virtual FK on tables with the most fk already
+			// a table without foreign keys will ensure the tool can run with no additional actions
+			if len(tables[targetTableIdx].Constraints) > len(tables[tableIdx].Constraints) {
+				table = tables[targetTableIdx]
+			} else {
+				table = tables[tableIdx]
+			}
+		case tableIdx != -1:
+			table = tables[tableIdx]
+		case targetTableIdx != -1:
+			table = tables[targetTableIdx]
+		default:
+			log.Warn().Str("key", source).Str("value", target).Str("func", "AddVirtualFKs").Msg("none of those tables are loaded")
+			continue
+
+		}
+
+		constraint := Constraint{
+			ConstraintName:        "VirtualFK_" + targetCol,
+			ReferencedTableSchema: table.Schema, // assuming the schema is the same, good enough for now
+			ReferencedTableName:   sourceTable,
+			ColumnsName:           []string{targetCol},
+			ReferencedColumsName:  []string{sourceCol},
+		}
+		constraint.populateFields(table)
+		err := constraint.loadReferencedTable()
+		if err != nil {
+			return errors.Wrap(err, "AddVirtualFKs")
+		}
+		table.Constraints = append(table.Constraints, constraint)
+		delete(fkeys, source)
+
+		// already called once on the real constraints, but it's safe to call twice to resolve the virtual FKs
+		table.resolveColToConstraints()
+	}
+
+	return nil
+}
+
+func (f *Field) skippeable() bool {
+	log.Debug().Str("field", f.ColumnName).Bool("nullable", f.IsNullable).Bool("hasDefault", f.HasDefaultValue).Str("func", "skippeable").Msg("debug skippeable")
+	if !f.IsNullable && !f.HasDefaultValue {
+		return false
+	}
+	return true
 }
